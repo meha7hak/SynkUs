6b5a08e5b4d2d72e08528f41ac3265af42cdf262
meha7hak <mehak.kotarya786@gmail.com>
2025-11-15 20:23:42 +0530

    backend runnig and setup

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://127.0.0.1:5173",
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+// test route
+app.get("/", (req, res) => {
+  res.json({ message: "SYNK US Backend Running" });
+});
+
+export default app;
---
+++ server/src/server.js
@@ -0,0 +1,39 @@
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL || "http://127.0.0.1:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connected:", socket.id);
+
+  socket.on("ping_from_client", (data) => {
+    console.log("ping_from_client:", data);
+    socket.emit("pong_from_server", { msg: "pong", ts: Date.now() });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
+  });
+});
+
+// connect to DB and start server
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log(`SYNK US backend running on http://127.0.0.1:${PORT}`);
+  });
+});
